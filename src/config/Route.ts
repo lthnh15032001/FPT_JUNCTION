d0bf7e5c56771bad63a6a8042a2c0c9dcbe3fae6
lthnh15032001 <lthnh15032001@gmail.com>
2021-12-10 21:59:57 +0700

    update dasshboard

--- src/config/Route.ts
+++ src/config/Route.ts
@@ -4,6 +4,6 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 router.get('/', function (req: Request, res: Response) {
     const url = process.env.NODE_ENV === "production" ? "https://mq-vnaa01.rogo.com.vn/v1/rogo" : "http://localhost:3000/"
-    res.render(path.join(__dirname, '../views/login.ejs'), { URL: url, voice_system: process.env.VOICE_SYSTEM || "alexa", key: res.locals['key'] });
+    res.render(path.join(__dirname, '../views/dashboard.ejs'), { URL: url, voice_system: process.env.VOICE_SYSTEM || "alexa", key: res.locals['key'] });
 });
 export default router
