689e668fb847605a42bd46440a65c2b4e4ac22ca
lthnh15032001 <lthnh15032001@gmail.com>
2021-12-11 03:18:39 +0700

    icon adding + api vantage

--- src/index.ts
+++ src/index.ts
@@ -43,13 +43,28 @@ app.get('/ping', async (req: Request, res: Response) => {
                 Authorization: "Bearer EfE0kQM8GaUbhyG7VealM71MiyoGl_Ld41qDhrc3RWA"
             }
         })
-       
+
         res.status(200).json(providers.data)
     } catch (e) {
         res.status(400).json({ e: e })
     }
 })
 // FPT server
+app.get('/getInfoOrg/:type', async (req: Request, res: Response) => {
+    // account_id
+    // region
+    // service
+    try {
+        const data = await axios.get('https://console.vantage.sh/reports/attribute_values?name=' + req.params.type, {
+            headers: {
+                cookie: 'intercom-session-gzqbquk3=bU9uVlVURFByOFgxU0JVTnAyMHZlK3Jid0l2SG9pNDhDQlYxbmVVak1lcHlWRUk5dEFPRExzVm45NVExcmdUZy0tTU1aeTZVMGR4V0xJNGVKZGUzN05YUT09--612da31b9bb42b5b576c6212fe8ade78d3973d28; _stratus_session=xXM3ujgFAk4K%2Bt5WLFEJ7XB7Wtg2f%2F6FgXGch4rocFNVteTWRVYc0lF2%2FXo337AIc%2Fe2x33cZ8e2A%2FvPQeQWWDTRViTUcVg5Y%2BvJW9X27P8xHAYZBdA%2F0CIrTVwPLpPuz7HGGJkHdyGdm8X1B7%2Ft%2FBVW2J1gBW7Rn5DLzFbhgwly9ggTApeI%2FtESm3ltfzZjBeQRdsFIw6NzVGA0cVYcXLr%2FK9VNT9yzT2OCtkn%2FazqzjUNiswbuvXmN%2FK0nACo8gFPMUH7ss2b1YrV0VGIjtL1YRSWWmdmd0zKAD6ri2OhkKNvIt8mWt9TIFRfdr3h8R1vEOCN6%2Fr2LoAOe6NkyAo7VB7gcfHg1KJnOYxHtfmPcmzawspa2JOUSwEvHSKvjdaPMx1kCAIOqMWAeTAaEC%2BzE130td%2F17KhlnkL%2Bcq6aD5hj5IhTnE0wbF5e%2BUWkk--F5ItBnzn%2Ba5TckN5--w8xrp1qY091e4iGuW9kaYw%3D%3D'
+            }
+        })
+        res.json(data.data)
+    } catch (e) {
+        res.status(400).json({ e: e })
+    }
+})
 
 // alexa skills + GA fullfillment + OAUTH2
 app.use('/prices', appRoutePrices)
